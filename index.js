af163723acb7605edb653ac7daa17591c99b139c
marcus <marcushurn@hotmail.com>
2020-12-01 18:31:46 -0800

    improved error handling (routes)

--- index.js
+++ index.js
@@ -8,74 +8,78 @@ const surveyMongo = require('./surveydb/surveyMongo');
 
 app.use(cors());
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use((error, req, res, next) => {
+  return res.status(500).json({ error: error.toString() });
+});
 
 app.get('/', (req, res) => {
   res.send('Survey API')
 })
 
-app.get('/allSurveys', (req, res) => {
+app.get('/allSurveys', (req, res, next) => {
   surveyService.getAllSurveys()
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 });
 
 /**
  * Retrieves all of a particular kind of survey.
  * Only completed surveys will be returned.
  */
-app.post('/surveyByType', (req, res) => {
+app.post('/surveyByType', (req, res, next) => {
   surveyService.getBySurveyName(req.body.typeName)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 });
 
 /**
  * Retuns a single blank survey given
  * the name of the survey.
  */
-app.post('/blankSurveyByType', (req, res) => {
+app.post('/blankSurveyByType', (req, res, next) => {
   surveyService.getBlankSurveyByName(req.body.typeName)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 })
 
 /**
  * Return a personal (answered) survey
  * for the given owner and survey.
  */
-app.post('/surveyByOwner', (req, res) => {
+app.post('/surveyByOwner', (req, res, next) => {
   surveyService.getSurveyByOwner(req.body.owner, req.body.survey)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 });
 
 /**
  * Save a survey. Returns the saved document.
  */
-app.post('/survey', (req, res) => {
+app.post('/survey', (req, res, next) => {
   surveyService.saveSurvey(req.body)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 })
 
 /**
  * Call with an updated survey, it will
  * replace an existing survey.
  */
-app.post('/update', (req, res) => {
+app.post('/update', (req, res, next) => {
   surveyService.updateBySurveyName(req.body)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 })
 
 /**
  * Delete ALL surveys by name
  */
-app.delete('/surveys', (req, res) => {
+app.delete('/surveys', (req, res, next) => {
   surveyService.deleteSurveysByName(req.body.name)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 })
 
 /**
@@ -83,10 +87,10 @@ app.delete('/surveys', (req, res) => {
  * to fill out.  Takes the participant's
  * id as a parameter.
  */
-app.post('/blankSurveyFor', (req, res) => {
+app.post('/blankSurveyFor', (req, res, next) => {
   surveyService.getBlankSurveyFor(req.body.participantId)
     .then( result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 })
 
 /**
@@ -94,21 +98,21 @@ app.post('/blankSurveyFor', (req, res) => {
  * Takes an array of participant names along
  * with the survey name.
  */
-app.post('/participant', (req, res) => {
+app.post('/participant', (req, res, next) => {
   surveyService
     .addSurveyParticipants(req.body.surveyName, req.body.names)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 })
 
 /**
  * Return a list of participants for a 
  * particular survey
  */
-app.post('/surveyTakers', (req, res) => {
+app.post('/surveyTakers', (req, res, next) => {
   surveyService.getParticipantsBySurvey(req.body.surveyName)
     .then(result => res.send(result))
-    .catch(err => console.error(err));
+    .catch(next);
 })
 
 
